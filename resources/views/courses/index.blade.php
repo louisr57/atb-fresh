dee8e434097033a161b4b9e6c567575b66d85184
louisr57 <louisr57@gmail.com>
2024-09-28 18:33:41 +0530

    Next commit

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -16,19 +16,31 @@
         <table class="min-w-full table-auto border-collapse border border-gray-300">
             <thead class="bg-gray-100">
                 <tr>
-                    <th class="border px-4 py-2">ID</th>
-                    <th class="border px-4 py-2">Course Title</th>
-                    <th class="border px-4 py-2">Duration</th>
-                    <th class="border px-4 py-2">Description</th>
+                    <th class="border border-gray-300 px-4 py-2 text-left">
+                        <a href="{{ route('courses.index', ['sort_by' => 'id', 'direction' => $direction === 'asc' ? 'desc' : 'asc']) }}"
+                            class="text-blue-500 hover:underline">ID</a>
+                    </th>
+                    <th class="border border-gray-300 px-4 py-2 text-left">
+                        <a href="{{ route('courses.index', ['sort_by' => 'course_title', 'direction' => $direction === 'asc' ? 'desc' : 'asc']) }}"
+                            class="text-blue-500 hover:underline">Course name</a>
+                    </th>
+                    <th class="border border-gray-300 px-4 py-2 text-left">
+                        <a href="{{ route('courses.index', ['sort_by' => 'duration', 'direction' => $direction === 'asc' ? 'desc' : 'asc']) }}"
+                            class="text-blue-500 hover:underline">Duration in Days</a>
+                    </th>
+                    <th class="border border-gray-300 px-4 py-2 text-left">
+                        <a href="{{ route('courses.index', ['sort_by' => 'description', 'direction' => $direction === 'asc' ? 'desc' : 'asc']) }}"
+                            class="text-blue-500 hover:underline">Description</a>
+                    </th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($courses as $course)
                 <tr class="hover:bg-gray-100">
-                    <td class="border px-4 py-2">{{ $course->id }}</td>
-                    <td class="border px-4 py-2">{{ $course->course_title }}</td>
-                    <td class="border px-4 py-2">{{ $course->duration }}</td>
-                    <td class="border px-4 py-2">{{ $course->description }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-left">{{ $course->id }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-left">{{ $course->course_title }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-left">{{ $course->duration }}</td>
+                    <td class="border border-gray-300 px-4 py-2 text-left">{{ $course->description }}</td>
                 </tr>
                 @endforeach
             </tbody>
