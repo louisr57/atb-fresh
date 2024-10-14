b6305b2138cca298f13a0e4534f75dfee89b58c2
Louis Radakir <louisr57@gmail.com>
2024-10-14 21:19:47 +0530

    Add Livewire manifest file. Added Blade UI Kit. Added onboard Tailwind with Vite JS implementation.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+// tailwind.config.js
+module.exports = {
+    content: [
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
+        "./resources/js/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
