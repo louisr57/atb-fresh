0061b71949a612c780b6bd9dd9e995d8f74d9ef5
Louis Radakir <louisr57@gmail.com>
2024-10-22 13:11:24 +0530

    Create venues table using some of events table entries

---
+++ database/migrations/2024_10_22_070254_create_venues_table.php.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venues', function (Blueprint $table) {
+            $table->id();
+            $table->string('venue_name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('postcode');
+            $table->string('location_geocode')->nullable();
+            $table->unsignedBigInteger('event_id');
+            $table->text('remarks')->nullable();
+            $table->timestamps();
+
+            // Add foreign key constraints
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('venues');
+    }
+};
