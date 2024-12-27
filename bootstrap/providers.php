3446846972f4198450fecf21ae97d03c307832e5
Louis Radakir <louisr57@gmail.com>
2024-12-27 14:57:32 +0530

    Debugging Telescope provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    // App\Providers\TelescopeServiceProvider::class, // Temporarily disabled until fixed
     Barryvdh\Debugbar\ServiceProvider::class,
 ];
