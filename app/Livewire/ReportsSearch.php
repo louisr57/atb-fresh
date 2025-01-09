1c9bd3d25864c81bf2f8be8118d4ab87e5b82d58
Louis Radakir <louisr57@gmail.com>
2025-01-09 21:48:11 +0530

    Tweak the search algorithm so that selection from one dropdown field has a filtering effect on subsequent other-field dropdown selections.

--- app/Livewire/ReportsSearch.php
+++ app/Livewire/ReportsSearch.php
@@ -126,16 +126,114 @@ class ReportsSearch extends Component
 
         $results = $query->paginate(10);
 
+        // Get filtered options for each dropdown
+        $baseQuery = Registration::query()
+            ->join('students', 'registrations.student_id', '=', 'students.id')
+            ->join('events', 'registrations.event_id', '=', 'events.id')
+            ->join('courses', 'events.course_id', '=', 'courses.id')
+            ->join('venues', 'events.venue_id', '=', 'venues.id');
+
+        // Apply current filters to each dropdown's options query
+        $coursesQuery = clone $baseQuery;
+        $statusesQuery = clone $baseQuery;
+        $studentCitiesQuery = clone $baseQuery;
+        $studentCountriesQuery = clone $baseQuery;
+        $venueNamesQuery = clone $baseQuery;
+        $venueCitiesQuery = clone $baseQuery;
+        $venueStatesQuery = clone $baseQuery;
+        $venueCountriesQuery = clone $baseQuery;
+
+        // Apply filters for each query except its own field
+        if ($this->search_course) {
+            $statusesQuery->where('courses.course_title', 'like', '%' . $this->search_course . '%');
+            $studentCitiesQuery->where('courses.course_title', 'like', '%' . $this->search_course . '%');
+            $studentCountriesQuery->where('courses.course_title', 'like', '%' . $this->search_course . '%');
+            $venueNamesQuery->where('courses.course_title', 'like', '%' . $this->search_course . '%');
+            $venueCitiesQuery->where('courses.course_title', 'like', '%' . $this->search_course . '%');
+            $venueStatesQuery->where('courses.course_title', 'like', '%' . $this->search_course . '%');
+            $venueCountriesQuery->where('courses.course_title', 'like', '%' . $this->search_course . '%');
+        }
+
+        if ($this->search_status) {
+            $coursesQuery->where('registrations.end_status', 'like', '%' . $this->search_status . '%');
+            $studentCitiesQuery->where('registrations.end_status', 'like', '%' . $this->search_status . '%');
+            $studentCountriesQuery->where('registrations.end_status', 'like', '%' . $this->search_status . '%');
+            $venueNamesQuery->where('registrations.end_status', 'like', '%' . $this->search_status . '%');
+            $venueCitiesQuery->where('registrations.end_status', 'like', '%' . $this->search_status . '%');
+            $venueStatesQuery->where('registrations.end_status', 'like', '%' . $this->search_status . '%');
+            $venueCountriesQuery->where('registrations.end_status', 'like', '%' . $this->search_status . '%');
+        }
+
+        if ($this->search_student_city) {
+            $coursesQuery->where('students.city', 'like', '%' . $this->search_student_city . '%');
+            $statusesQuery->where('students.city', 'like', '%' . $this->search_student_city . '%');
+            $studentCountriesQuery->where('students.city', 'like', '%' . $this->search_student_city . '%');
+            $venueNamesQuery->where('students.city', 'like', '%' . $this->search_student_city . '%');
+            $venueCitiesQuery->where('students.city', 'like', '%' . $this->search_student_city . '%');
+            $venueStatesQuery->where('students.city', 'like', '%' . $this->search_student_city . '%');
+            $venueCountriesQuery->where('students.city', 'like', '%' . $this->search_student_city . '%');
+        }
+
+        if ($this->search_student_country) {
+            $coursesQuery->where('students.country', 'like', '%' . $this->search_student_country . '%');
+            $statusesQuery->where('students.country', 'like', '%' . $this->search_student_country . '%');
+            $studentCitiesQuery->where('students.country', 'like', '%' . $this->search_student_country . '%');
+            $venueNamesQuery->where('students.country', 'like', '%' . $this->search_student_country . '%');
+            $venueCitiesQuery->where('students.country', 'like', '%' . $this->search_student_country . '%');
+            $venueStatesQuery->where('students.country', 'like', '%' . $this->search_student_country . '%');
+            $venueCountriesQuery->where('students.country', 'like', '%' . $this->search_student_country . '%');
+        }
+
+        if ($this->search_venue_name) {
+            $coursesQuery->where('venues.venue_name', 'like', '%' . $this->search_venue_name . '%');
+            $statusesQuery->where('venues.venue_name', 'like', '%' . $this->search_venue_name . '%');
+            $studentCitiesQuery->where('venues.venue_name', 'like', '%' . $this->search_venue_name . '%');
+            $studentCountriesQuery->where('venues.venue_name', 'like', '%' . $this->search_venue_name . '%');
+            $venueCitiesQuery->where('venues.venue_name', 'like', '%' . $this->search_venue_name . '%');
+            $venueStatesQuery->where('venues.venue_name', 'like', '%' . $this->search_venue_name . '%');
+            $venueCountriesQuery->where('venues.venue_name', 'like', '%' . $this->search_venue_name . '%');
+        }
+
+        if ($this->search_venue_city) {
+            $coursesQuery->where('venues.city', 'like', '%' . $this->search_venue_city . '%');
+            $statusesQuery->where('venues.city', 'like', '%' . $this->search_venue_city . '%');
+            $studentCitiesQuery->where('venues.city', 'like', '%' . $this->search_venue_city . '%');
+            $studentCountriesQuery->where('venues.city', 'like', '%' . $this->search_venue_city . '%');
+            $venueNamesQuery->where('venues.city', 'like', '%' . $this->search_venue_city . '%');
+            $venueStatesQuery->where('venues.city', 'like', '%' . $this->search_venue_city . '%');
+            $venueCountriesQuery->where('venues.city', 'like', '%' . $this->search_venue_city . '%');
+        }
+
+        if ($this->search_venue_state) {
+            $coursesQuery->where('venues.state', 'like', '%' . $this->search_venue_state . '%');
+            $statusesQuery->where('venues.state', 'like', '%' . $this->search_venue_state . '%');
+            $studentCitiesQuery->where('venues.state', 'like', '%' . $this->search_venue_state . '%');
+            $studentCountriesQuery->where('venues.state', 'like', '%' . $this->search_venue_state . '%');
+            $venueNamesQuery->where('venues.state', 'like', '%' . $this->search_venue_state . '%');
+            $venueCitiesQuery->where('venues.state', 'like', '%' . $this->search_venue_state . '%');
+            $venueCountriesQuery->where('venues.state', 'like', '%' . $this->search_venue_state . '%');
+        }
+
+        if ($this->search_venue_country) {
+            $coursesQuery->where('venues.country', 'like', '%' . $this->search_venue_country . '%');
+            $statusesQuery->where('venues.country', 'like', '%' . $this->search_venue_country . '%');
+            $studentCitiesQuery->where('venues.country', 'like', '%' . $this->search_venue_country . '%');
+            $studentCountriesQuery->where('venues.country', 'like', '%' . $this->search_venue_country . '%');
+            $venueNamesQuery->where('venues.country', 'like', '%' . $this->search_venue_country . '%');
+            $venueCitiesQuery->where('venues.country', 'like', '%' . $this->search_venue_country . '%');
+            $venueStatesQuery->where('venues.country', 'like', '%' . $this->search_venue_country . '%');
+        }
+
         return view('livewire.reports-search', [
             'results' => $results,
-            'courses' => Course::query()->pluck('course_title'),
-            'statuses' => Registration::query()->select('end_status')->distinct()->pluck('end_status'),
-            'studentCities' => Student::query()->select('city')->distinct()->pluck('city'),
-            'studentCountries' => Student::query()->select('country')->distinct()->pluck('country'),
-            'venueNames' => Venue::query()->select('venue_name')->distinct()->pluck('venue_name'),
-            'venueCities' => Venue::query()->select('city')->distinct()->pluck('city'),
-            'venueStates' => Venue::query()->select('state')->distinct()->pluck('state'),
-            'venueCountries' => Venue::query()->select('country')->distinct()->pluck('country')
+            'courses' => $coursesQuery->select('courses.course_title')->distinct()->pluck('course_title'),
+            'statuses' => $statusesQuery->select('registrations.end_status')->distinct()->pluck('end_status'),
+            'studentCities' => $studentCitiesQuery->select('students.city')->distinct()->pluck('city'),
+            'studentCountries' => $studentCountriesQuery->select('students.country')->distinct()->pluck('country'),
+            'venueNames' => $venueNamesQuery->select('venues.venue_name')->distinct()->pluck('venue_name'),
+            'venueCities' => $venueCitiesQuery->select('venues.city')->distinct()->pluck('city'),
+            'venueStates' => $venueStatesQuery->select('venues.state')->distinct()->pluck('state'),
+            'venueCountries' => $venueCountriesQuery->select('venues.country')->distinct()->pluck('country')
         ]);
     }
 }
