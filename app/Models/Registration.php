4b6cb516dae56e8c1e55a1b851a5bf7d1fc66dd9
Louis Radakir <louisr57@gmail.com>
2025-01-12 20:06:35 +0530

    Fix record navigator # of x count indication ... where 1 of x could not be shown and the last record would show (x+1) of x!

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -30,31 +30,60 @@ class Registration extends Model
 
     public function nextInEvent()
     {
-        return $this->where('event_id', $this->event_id)
-            ->where('id', '>', $this->id)
-            ->orderBy('id', 'asc')
+        return $this->join('students', 'registrations.student_id', '=', 'students.id')
+            ->where('registrations.event_id', $this->event_id)
+            ->where(function($query) {
+                $query->whereRaw("CONCAT(students.first_name, ' ', students.last_name) > ?",
+                    [$this->student->first_name . ' ' . $this->student->last_name])
+                ->orWhere(function($q) {
+                    $q->whereRaw("CONCAT(students.first_name, ' ', students.last_name) = ?",
+                        [$this->student->first_name . ' ' . $this->student->last_name])
+                    ->where('registrations.id', '>', $this->id);
+                });
+            })
+            ->orderByRaw("CONCAT(students.first_name, ' ', students.last_name) ASC")
+            ->orderBy('registrations.id', 'asc')
+            ->select('registrations.*')
             ->first();
     }
 
     public function previousInEvent()
     {
-        return $this->where('event_id', $this->event_id)
-            ->where('id', '<', $this->id)
-            ->orderBy('id', 'desc')
+        return $this->join('students', 'registrations.student_id', '=', 'students.id')
+            ->where('registrations.event_id', $this->event_id)
+            ->where(function($query) {
+                $query->whereRaw("CONCAT(students.first_name, ' ', students.last_name) < ?",
+                    [$this->student->first_name . ' ' . $this->student->last_name])
+                ->orWhere(function($q) {
+                    $q->whereRaw("CONCAT(students.first_name, ' ', students.last_name) = ?",
+                        [$this->student->first_name . ' ' . $this->student->last_name])
+                    ->where('registrations.id', '<', $this->id);
+                });
+            })
+            ->orderByRaw("CONCAT(students.first_name, ' ', students.last_name) DESC")
+            ->orderBy('registrations.id', 'desc')
+            ->select('registrations.*')
             ->first();
     }
 
     public function getPositionInEvent()
     {
-        $position = $this->where('event_id', $this->event_id)
-            ->where('id', '<=', $this->id)
-            ->count();
+        // Get all registrations for this event ordered by student name
+        $orderedRegistrations = $this->join('students', 'registrations.student_id', '=', 'students.id')
+            ->where('registrations.event_id', $this->event_id)
+            ->orderByRaw("CONCAT(students.first_name, ' ', students.last_name) ASC")
+            ->orderBy('registrations.id', 'asc')
+            ->select('registrations.*')
+            ->get();
 
-        $total = $this->where('event_id', $this->event_id)->count();
+        // Find position of current registration (1-based index)
+        $position = $orderedRegistrations->search(function($item) {
+            return $item->id === $this->id;
+        }) + 1;
 
         return [
             'current' => $position,
-            'total' => $total
+            'total' => $orderedRegistrations->count()
         ];
     }
 }
