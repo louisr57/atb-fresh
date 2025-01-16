e7c6af85fdad36ea762b2323aaa585a66cca26c3
Louis Radakir <louisr57@gmail.com>
2025-01-16 18:52:12 +0530

    Remove comments.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,1 @@
 import "./bootstrap";
-
-// Below removed on recommendation from https://livewire.laravel.com/docs/quickstart
-// import Alpine from "alpinejs";
-// import persist from "@alpinejs/persist";
-
-// // Initialize Alpine plugins
-// Alpine.plugin(persist);
-
-// window.Alpine = Alpine;
-// Alpine.start();
