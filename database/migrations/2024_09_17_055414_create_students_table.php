d031f1c12f8841211bbd565acf3d49b3bbcbb986
Louis Radakir <louisr57@gmail.com>
2025-01-22 14:52:27 +0530

    Make the phone number nullable

--- database/migrations/2024_09_17_055414_create_students_table.php
+++ database/migrations/2024_09_17_055414_create_students_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('gender')->nullable();
             $table->string('email')->unique();
-            $table->string('phone_number')->unique();
+            $table->string('phone_number')->nullable();
             $table->string('address');
             $table->string('city');
             $table->string('state');
